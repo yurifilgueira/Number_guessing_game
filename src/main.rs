69273e5f49c331374f07dd106f7f9bbc84698cdf
Yuri FTM <yurimedeiros141@gmail.com>
2024-03-19 22:47:27 -0300

    Project done

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,45 @@
-use std::io::{self, Read};
+use std::io::{self, Write};
 
 use rand::Rng;
 
-fn main() {
+fn main() -> io::Result<()>{
     
     let mut rng = rand::thread_rng();
 
     let number: i32 = rng.gen_range(1..=100);
 
-    while true {
+    while number >= 0 {
         print!("Insert a number between 1 and 100: ");
+        io::stdout().flush()?;
         let mut guess = String::new();
 
         io::stdin().read_line(&mut guess)
                             .expect("Fail to read input");
 
-    }
 
+        match guess.trim().parse::<i32>() {
+            Ok(choice) => {
+                if choice < 0 || choice > 100 {
+                    println!("The number must be between 0 and 100! Try again.")
+                }
+                else if choice > number {
+                    println!("The number is smaller. Try again!");
+                }
+                else if choice < number {
+                    println!("The number is greater. Try again!");
+                }
+                else {
+                    println!("Congratulations!");
+                    
+                    break;
+                }
+            }
+            Err(_) => {
+                println!("Invalid input. Please enter a valid number.\n");
+                continue;
+            }
+        }
+    }
+    
+    Ok(())
 }
